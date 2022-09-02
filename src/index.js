1f7c2c5ab456e919026fa9e78e572ae60fb1f2a7
Anubhav Anand <anubhavanandgolu@gmail.com>
2022-09-02 20:37:03 +0530

    first push

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// const fname="Anubhav";
+// const lname="Anand";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <App/>
+);
+
+reportWebVitals();
+// var React=require("react");
+// var ReactDOM=require("react-dom");
+// ReactDOM.render(
+// <h1>Hello World!!!</h1>,
+// document.getElementById("root"));
